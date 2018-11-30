3feafe4d87499802a4345157c62c927b2a2d35a5
rofinn <rory.finnegan@gmail.com>
2018-11-30 12:09:37 -0600

    Initial working copy of the FTP server from FTPClient.jl

--- src/FTPLib.jl
+++ src/FTPLib.jl
@@ -1,5 +1,126 @@
 module FTPLib
 
-greet() = print("Hello World!")
+import Base: Process
+import Base: close
+using Conda
+using Compat
+using Compat: @__MODULE__
+using Compat.Random: randstring
+using Memento
+using PyCall
+const LOGGER = getlogger(@__MODULE__)
+
+const pylogging = PyNULL()
+const pyopenssl_crypto = PyNULL()
+const pyopenssl_SSL = PyNULL()
+const pyftpdlib_authorizers = PyNULL()
+const pyftpdlib_handlers = PyNULL()
+const pyftpdlib_servers = PyNULL()
+
+# Defaults from pyftpdlib example
+const USER = "user"
+const PASSWD = "12345"
+const HOST="localhost"
+const PORT = 2021
+const PERM = "elradfmwM"
+const DEBUG = false
+
+const SCRIPT = abspath(dirname(@__FILE__), "server.py")
+const ROOT = realpath(abspath(joinpath(dirname(dirname(@__FILE__)), "usr", "ftp")))
+const HOMEDIR = joinpath(ROOT, "data")
+const CERT = joinpath(ROOT, "test.crt")
+const KEY = joinpath(ROOT, "test.key")
+const PYTHON_CMD = joinpath(
+    Conda.PYTHONDIR, Compat.Sys.iswindows() ? "python.exe" : "python"
+)
+
+function __init__()
+    Memento.register(LOGGER)
+    copy!(pyopenssl_crypto, pyimport_conda("OpenSSL.crypto", "OpenSSL"))
+    copy!(pyopenssl_SSL, pyimport_conda("OpenSSL.SSL", "OpenSSL"))
+    copy!(pyftpdlib_servers, pyimport_conda("pyftpdlib.servers", "pyftpdlib"))
+
+    DEBUG && pylogging[:basicConfig](level=pylogging[:DEBUG])
+    mkpath(HOMEDIR)
+end
+
+mutable struct FTPServer
+    root::AbstractString
+    port::Int
+    username::AbstractString
+    password::AbstractString
+    permissions::AbstractString
+    security::Symbol
+    process::Process
+    io::IO
+
+     function FTPServer(
+        root::AbstractString=ROOT; username="", password="", permissions="elradfmwM",
+        security::Symbol=:none,
+    )
+        if isempty(username)
+            username = string("user", rand(1:9999))
+        end
+        if isempty(password)
+            password = randstring(40)
+        end
+
+        cmd = `$PYTHON_CMD $SCRIPT $username $password $root --permissions $permissions`
+        if security != :none
+            cmd = `$cmd --tls $security --cert-file $CERT --key-file $KEY --gen-certs TRUE`
+        end
+        io = Pipe()
+
+        # Note: open(::AbstractCmd, ...) won't work here as it doesn't allow us to capture STDERR.
+        process = if VERSION > v"0.7.0-DEV.4445"
+            run(pipeline(cmd, stdout=io, stderr=io), wait=false)
+        else
+            spawn(pipeline(cmd, stdout=io, stderr=io))
+        end
+
+        line = readline(io)
+        m = match(r"starting FTP.* server on .*:(?<port>\d+)", line)
+        if m !== nothing
+            port = parse(Int, m[:port])
+            new(root, port, username, password, permissions, security, process, io)
+        else
+            kill(process)
+            error(line, String(readavailable(io)))  # Display traceback
+        end
+    end
+end
+
+hostname(server::FTPServer) = "localhost"
+port(server::FTPServer) = server.port
+username(server::FTPServer) = server.username
+password(server::FTPServer) = server.password
+close(server::FTPServer) = kill(server.process)
+
+localpath(server::FTPServer, path::AbstractString) = joinpath(server.root, split(path, '/')...)
+
+function tempfile(path::AbstractString)
+    content = randstring(rand(1:100))
+    open(path, "w") do fp
+        write(fp, content)
+    end
+    return content
+end
+
+function setup_root(dir::AbstractString)
+    mkdir(dir)
+    tempfile(joinpath(dir, "test_download.txt"))
+    tempfile(joinpath(dir, "test_download2.txt"))
+    mkdir(joinpath(dir, "test_directory"))
+end
+
+function setup_server()
+    isdir(joinpath(FTPLib.ROOT, "data")) || setup_root(FTPLib.ROOT)
+end
+
+function teardown_server()
+    rm(FTPLib.ROOT, recursive=true)
+    isfile(FTPLib.CERT) && rm(FTPLib.CERT)
+    isfile(FTPLib.KEY) && rm(FTPLib.KEY)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,52 @@
+using Compat
+using Compat.Test
 using FTPLib
-using Test
+using FTPClient
+
 
 @testset "FTPLib.jl" begin
-    # Write your own tests here.
+    @testset "no-ssl" begin
+        FTPLib.setup_server()
+        server = FTPLib.FTPServer()
+
+        opts = (
+            :hostname => FTPLib.hostname(server),
+            :port => FTPLib.port(server),
+            :username => FTPLib.username(server),
+            :password => FTPLib.password(server),
+        )
+
+        try
+            options = RequestOptions(; opts..., ssl=false)
+            ctxt, resp = ftp_connect(options)
+            @test resp.code == 226
+        finally
+            close(server)
+        end
+    end
+    @testset "ssl - $mode" for mode in (:explicit, :implicit)
+        FTPLib.setup_server()
+        server = FTPLib.FTPServer(security=mode)
+
+        opts = (
+            :hostname => FTPLib.hostname(server),
+            :port => FTPLib.port(server),
+            :username => FTPLib.username(server),
+            :password => FTPLib.password(server),
+            :ssl => true,
+            :implicit => mode === :implicit,
+            :verify_peer => false,
+        )
+
+        try
+            options = RequestOptions(; opts...)
+            # Test implicit/exlicit ftp ssl scheme is set correctly
+            @test options.uri.scheme == (mode === :implicit ? "ftps" : "ftpes")
+            ctxt, resp = ftp_connect(options)
+            @test resp.code == 226
+        finally
+            close(server)
+        end
+    end
+
 end
