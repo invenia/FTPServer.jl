bad1738839d586e251f499344cb504ab5e8b200a
rofinn <rory.finnegan@gmail.com>
2018-12-03 15:03:20 -0600

    Added some doc strings.

--- src/FTPServer.jl
+++ src/FTPServer.jl
@@ -41,8 +41,9 @@ function __init__()
     copy!(pyftpdlib_servers, pyimport_conda("pyftpdlib.servers", "pyftpdlib", "invenia"))
 
     DEBUG && pylogging[:basicConfig](level=pylogging[:DEBUG])
-    mkpath(HOMEDIR)
+    mkpath(ROOT)
 end
+
 
 mutable struct Server
     root::AbstractString
@@ -53,44 +54,66 @@ mutable struct Server
     security::Symbol
     process::Process
     io::IO
+end
 
-     function Server(
-        root::AbstractString=ROOT; username="", password="", permissions="elradfmwM",
-        security::Symbol=:none,
-    )
-        if isempty(username)
-            username = string("user", rand(1:9999))
-        end
-        if isempty(password)
-            password = randstring(40)
-        end
+"""
+    Server(root=$ROOT; username=$USER, password=$PASSWD, permissions=$PERM, security=:none)
 
-        cmd = `$PYTHON_CMD $SCRIPT $username $password $root --permissions $permissions`
-        if security != :none
-            cmd = `$cmd --tls $security --cert-file $CERT --key-file $KEY --gen-certs TRUE`
-        end
-        io = Pipe()
+A Server stores settings for create an pyftpdlib server.
 
-        # Note: open(::AbstractCmd, ...) won't work here as it doesn't allow us to capture STDERR.
-        process = if VERSION > v"0.7.0-DEV.4445"
-            run(pipeline(cmd, stdout=io, stderr=io), wait=false)
-        else
-            spawn(pipeline(cmd, stdout=io, stderr=io))
-        end
+# Arguments
+- `root::AbstractString`: Directory where you want store credentials and data for the
+  test server.
 
-        line = readline(io)
-        m = match(r"starting FTP.* server on .*:(?<port>\d+)", line)
-        if m !== nothing
-            port = parse(Int, m[:port])
-            new(root, port, username, password, permissions, security, process, io)
-        else
-            kill(process)
-            error(line, String(readavailable(io)))  # Display traceback
-        end
+# Keywords
+- `username`: Default login username
+- `password`: Default login password
+- `permission`: Default user read/write permissions
+- `security`: Security method to use for connecting (options: `:none`, `:implicity`, `:explicity`).
+  Passing in `:none` will use FTP and passing in `:implicity` or `:explicity` will use the appropriate
+  FTPS connection.
+"""
+function Server(
+    root::AbstractString=ROOT; username="", password="", permissions="elradfmwM",
+    security::Symbol=:none,
+)
+    if isempty(username)
+        username = string("user", rand(1:9999))
+    end
+    if isempty(password)
+        password = randstring(40)
+    end
+
+    cmd = `$PYTHON_CMD $SCRIPT $username $password $root --permissions $permissions`
+    if security != :none
+        cmd = `$cmd --tls $security --cert-file $CERT --key-file $KEY --gen-certs TRUE`
+    end
+    io = Pipe()
+
+    # Note: open(::AbstractCmd, ...) won't work here as it doesn't allow us to capture STDERR.
+    process = if VERSION > v"0.7.0-DEV.4445"
+        run(pipeline(cmd, stdout=io, stderr=io), wait=false)
+    else
+        spawn(pipeline(cmd, stdout=io, stderr=io))
+    end
+
+    line = readline(io)
+    m = match(r"starting FTP.* server on .*:(?<port>\d+)", line)
+    if m !== nothing
+        port = parse(Int, m[:port])
+        Server(root, port, username, password, permissions, security, process, io)
+    else
+        kill(process)
+        error(line, String(readavailable(io)))  # Display traceback
     end
 end
 
+"""
+    serve(f, args...; kwargs...)
 
+Passes `args` and `kwargs` to the `Server` constructor and runs the function `f` by passing
+in the `server` instance. Upon completion the `server` will automatically be shutdown.
+"""
 function serve(f::Function, args...; kwargs...)
     server = Server(args...; kwargs...)
 
@@ -109,17 +132,43 @@ close(server::Server) = kill(server.process)
 
 localpath(server::Server, path::AbstractString) = joinpath(server.root, split(path, '/')...)
 
-function setup_root(dir::AbstractString)
+function tempfile(path::AbstractString)
+    content = randstring(rand(1:100))
+    open(path, "w") do fp
+        write(fp, content)
+    end
+    return content
+end
+
+function setup_home(dir::AbstractString)
     mkdir(dir)
     tempfile(joinpath(dir, "test_download.txt"))
     tempfile(joinpath(dir, "test_download2.txt"))
     mkdir(joinpath(dir, "test_directory"))
 end
 
-function init()
-    isdir(joinpath(FTPServer.ROOT, "data")) || setup_root(FTPServer.ROOT)
-end
+"""
+    init()
 
+Creates a test $HOMEDIR with a few sample files if one hasn't already been setup.
+
+```
+$HOMEDIR/test_download.txt
+$HOMEDIR/test_download2.txt
+$HOMEDIR/test_directory/
+````
+"""
+init() = isdir(FTPServer.HOMEDIR) || setup_home(FTPServer.HOMEDIR)
+
+"""
+    cleanup()
+
+Cleans up the default FTPServer.ROOT directory:
+
+- $HOMEDIR
+- $CERT
+- $KEY
+"""
 function cleanup()
     rm(FTPServer.HOMEDIR, recursive=true)
     isfile(FTPServer.CERT) && rm(FTPServer.CERT)
