505883376608743ebd5b9bca5107938e48ccc3e6
Curtis Vogt <curtis.vogt@gmail.com>
2019-01-09 15:29:41 -0600

    Remove unnecessary Python imports (#5)

--- src/server.py
+++ src/server.py
@@ -1,7 +1,4 @@
-from OpenSSL import crypto, SSL
-from socket import gethostname
-from pprint import pprint
-from time import gmtime, mktime
+from OpenSSL import crypto
 from os.path import exists, join, realpath
 
 import logging
