c511603cff84eef8e27cf38a0e141d176689ba8e
Fernando Chorney <fernando.chorney@invenia.ca>
2020-03-25 10:02:32 -0500

    Move openssl pinning to deps/build.jl

--- src/FTPServer.jl
+++ src/FTPServer.jl
@@ -29,13 +29,6 @@ const PYTHON_CMD = joinpath(
 
 function __init__()
     Memento.register(LOGGER)
-
-    # OpenSSL 1.1.1e added in a breaking change that is affecting many FTP clients.
-    # For now, we can pin to 1.1.1d until it is fixed.
-    # https://github.com/openssl/openssl/issues/11381
-    # https://github.com/openssl/openssl/issues/11378
-    Conda.add("openssl==1.1.1d")
-
     copy!(pyopenssl_crypto, pyimport_conda("OpenSSL.crypto", "OpenSSL"))
     copy!(pyopenssl_SSL, pyimport_conda("OpenSSL.SSL", "OpenSSL"))
     copy!(pyftpdlib_servers, pyimport_conda("pyftpdlib.servers", "pyftpdlib", "invenia"))
